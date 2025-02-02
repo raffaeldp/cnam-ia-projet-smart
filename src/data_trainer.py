79f8cf7fd7668e929589c859e2a803de5bde5c80
Raffael Di Pietro <maxou4170@gmail.com>
2025-02-02 02:30:19 +0100

    update training parameters

--- src/data_trainer.py
+++ src/data_trainer.py
@@ -13,13 +13,25 @@ class DataTrainer:
     def train(self):
         print("Starting data training")
         # Load the model
-        model = YOLO("yolo11n.pt")
+        model = YOLO("yolo11s.pt")
 
 
         # Train the model using the 'data.yaml' datasets for 3 epochs
 
         config_path = Path(DataPreprocessor.config_path).resolve()
-        results = model.train(data=config_path, epochs=3, device="mps")
+
+        results = model.train(
+            data=config_path,
+            device="mps",
+            imgsz=640,
+            epochs=50,
+            batch=16,
+            close_mosaic=False,
+            optimizer="adamW",
+            seed=42,
+            lr0=0.001,
+            lrf=0.001,
+        )
 
         # Evaluate the model's performance on the val set
         results = model.val()
