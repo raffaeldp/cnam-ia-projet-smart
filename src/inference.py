dc3634d3aa7d60e7f5f827db2d54dd27d2a4bcfd
Raffael Di Pietro <maxou4170@gmail.com>
2025-02-09 18:26:58 +0100

    feat(inference): do not re-download already downloaded model

--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 from picsellia import Model, ModelVersion, ModelFile
 from ultralytics import YOLO
@@ -27,8 +26,9 @@ class InferenceMode(Enum):
 
 class ImageProcessor:
     __model_download_directory_path = "./weights"
-    __model_zip_file_name = "best.zip"
-    __model_file_name = "best.pt"
+    __model_zip_file_path = f"{__model_download_directory_path}/best.zip"
+    __model_file_path = f"{__model_download_directory_path}/best.pt"
+    __model_latest_file_path = f"{__model_download_directory_path}/latest"
 
     def __select_model_version_idx(self, model: Model) -> int:
         model_versions: list[ModelVersion] = model.list_versions()
@@ -65,6 +65,27 @@ class ImageProcessor:
             return self.__select_model_file(model_version)
         return model_files[int(fileIdx)]
 
+    def __download_model(self, model_version: ModelVersion, model_file: ModelFile):
+        # Delete best.pt file if it exists
+        if os.path.exists(self.__model_file_path):
+            os.remove(self.__model_file_path)
+        if os.path.exists(self.__model_zip_file_path):
+            os.remove(self.__model_zip_file_path)
+        if os.path.exists(self.__model_latest_file_path):
+            os.remove(self.__model_latest_file_path)
+
+        model_file.download(self.__model_download_directory_path)
+        # Unzip the downloaded file
+        with zipfile.ZipFile(self.__model_zip_file_path, "r") as zip_ref:
+            zip_ref.extractall(self.__model_download_directory_path)
+
+        # Delete zip file with shutil
+        os.remove(self.__model_zip_file_path)
+
+        # Create a latest file with model_version name and model_file name
+        with open(self.__model_latest_file_path, "w") as f:
+            f.write(f"{model_version.name}\n{model_file.name}")
+
     def start_inference(
         self, mode: InferenceMode = InferenceMode.WEBCAM, path: str = None
     ) -> None:
@@ -78,40 +99,28 @@ class ImageProcessor:
         # Create download folder if not exists
         os.makedirs(self.__model_download_directory_path, exist_ok=True)
 
-        # Delete best.pt file if it exists
-        if os.path.exists(
-            f"{self.__model_download_directory_path}/{self.__model_file_name}"
-        ):
-            os.remove(
-                f"{self.__model_download_directory_path}/{self.__model_file_name}"
-            )
-        if os.path.exists(
-            f"{self.__model_download_directory_path}/{self.__model_zip_file_name}"
-        ):
-            os.remove(
-                f"{self.__model_download_directory_path}/{self.__model_zip_file_name}"
-            )
-
         picselliaConnector = PicselliaConnector()
         project_model = picselliaConnector.get_model()
         version_idx = self.__select_model_version_idx(project_model)
         version = project_model.list_versions()[version_idx].name
         model_version: ModelVersion = project_model.get_version(version)
         model_file: ModelFile = self.__select_model_file(model_version)
-        model_file.download(self.__model_download_directory_path)
 
-        # Unzip the downloaded file
-        with zipfile.ZipFile(
-            f"{self.__model_download_directory_path}/{self.__model_zip_file_name}", "r"
-        ) as zip_ref:
-            zip_ref.extractall(self.__model_download_directory_path)
+        # Check that the latest file do not contain the same model version and model file
+        if os.path.exists(self.__model_latest_file_path):
+            with open(self.__model_latest_file_path, "r") as f:
+                latest_model_version_name, latest_model_file_name = f.read().split("\n")
+                if (
+                    latest_model_version_name == model_version.name
+                    and latest_model_file_name == model_file.name
+                ):
+                    print("Model already downloaded")
+                else:
+                    self.__download_model(model_version, model_file)
+        else:
+            self.__download_model(model_version, model_file)
 
-        # Delete zip file with shutil
-        os.remove(
-            f"{self.__model_download_directory_path}/{self.__model_zip_file_name}"
-        )
-
-        model = YOLO(f"{self.__model_download_directory_path}/{self.__model_file_name}")
+        model = YOLO(self.__model_file_path)
 
         if mode == InferenceMode.WEBCAM:
             model(0, device=get_model_device(), show=True)
